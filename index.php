a7bfa8d2259519656838cbf9e15aba1b88715d9b
unknown <djenahalimjob@gmail.com>
2021-01-02 21:58:51 +0000

    adding welcome alert box

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<script>alert('Welcome to my website, please log in using:\r\nUsername: guest\r\nPassword: guest')
+          </script>;
 
 <head>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
