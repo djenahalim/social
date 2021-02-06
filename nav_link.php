ba3b374f9c1d7db29de1782359177446b1e2f2ce
unknown <djenahalimjob@gmail.com>
2021-02-06 00:47:13 +0000

    modified:   css/style.css 	modified:   css/style.scss 	modified:   nav_link.php

--- nav_link.php
+++ nav_link.php
@@ -8,11 +8,11 @@
        
 
         <li class="nav-item">
-          <a class="nav-link" id="posts" href="\posts\my_posts.php">Posts</a> 
+          <a class="nav-link" id="posts" href="\posts\my_posts.php">My friends posts</a> 
         </li>
 
         <li class="nav-item">
-        <a class="nav-link" id="my_posts" href="\posts\view_my_posts.php">View my posts</a> 
+        <a class="nav-link" id="my_posts" href="\posts\view_my_posts.php">My posts</a> 
       </li>
 
         <li class="nav-item">
